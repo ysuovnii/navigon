7d9380dd4bcf15a3d44a981143c498e54b30c23f
ysuovnii <ysuovnii@gmail.com>
2025-12-14 12:41:47 +0530

    init player config

---
+++ class/playerConfig.hpp
@@ -0,0 +1,13 @@
+#ifndef PLAYERCONFIG_HPP
+#define PLAYERCONFIG_HPP
+
+#include <SFML/Graphics.hpp>
+
+extern float PLAYER_SPEED;
+extern float PLAYER_VISION_RADIUS;
+extern float PLAYER_RADIUS;
+extern sf::Color PLAYER_COLOR;
+extern float PLAYER_INIT_X;
+extern float PLAYER_INIT_Y;
+
+#endif
---
+++ playerConfig.cpp
@@ -0,0 +1,8 @@
+#include "./class/playerConfig.hpp"
+
+float PLAYER_SPEED = 10.f;
+float PLAYER_VISION_RADIUS = 100.f;
+float PLAYER_RADIUS = 25.f;
+sf::Color PLAYER_COLOR = sf::Color::Red;
+float PLAYER_INIT_X = 100.f;
+float PLAYER_INIT_Y = 100.f;
